1b7aa09c9e343d76c39b312867e1d9309c1c5dfc
Stanislav Alimpov <stanislav.alimpov@vrpconsulting.com>
2020-10-06 16:08:04 +0300

    feat: implemented towel sorting code

--- src/index.js
+++ src/index.js
@@ -2,5 +2,7 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  return matrix ? matrix.reduce((result, item, i) => {
+    return result.concat(i % 2 === 1 ? item.reverse() : item);
+  }, []) : [];
 }
